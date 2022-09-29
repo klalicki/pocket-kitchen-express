bba9694f06e39c61baad79027951049d07439f39
Kristoff Lalicki <kristofflalicki@gmail.com>
2022-09-29 18:14:03 -0400

    adds server-side rendering to recipe list

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const app = express();
+
 const appConstants = {
     'recipePath': 'recipes'
 }
 
 const recipes = {};
+const recipeMeta =[];
 let recipesReady = false;
 
 let port = process.env.PORT;
@@ -15,7 +17,7 @@ if (port == null || port == "") {
 }
 app.set('view engine', 'ejs')
 app.get('/app', function (req, res) {
-    res.render('recipe-list');
+    res.render('recipe-list', {recipeMeta:recipeMeta});
 });
 app.use(express.static('public'))
 
@@ -29,13 +31,20 @@ async function loadRecipes() {
     //list all files in the recipes folder
     const fileList = fs.readdirSync(appConstants.recipePath);
     //iterate through all files and load their contents
-    fileList.forEach( function (filename) {
+    fileList.forEach(function (filename) {
         let fileText = fs.readFileSync(`${appConstants.recipePath}/${filename}`)
         let recipeSlug = path.basename(filename, '.json')
         let recipeObj = JSON.parse(fileText);
         recipes[recipeSlug] = recipeObj;
+        recipes[recipeSlug].meta.slug=recipeSlug;
     })
+    //build metadata object
+    for (const item in recipes){
+        recipeMeta.push(recipes[item].meta);
+        
+    }
+    console.log(recipeMeta);
+
 }
 
 loadRecipes();
-
