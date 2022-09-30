1ad30fb0aa902949d2a146d47da6104673fb229b
Kristoff Lalicki <kristofflalicki@gmail.com>
2022-09-30 19:40:07 -0400

    completed recipe page with SSR

--- public/app/scripts/recipe-ui-new.js
+++ public/app/scripts/recipe-ui-new.js
@@ -1,5 +1,6 @@
 import Swiper from 'swiper';
 // import Swiper styles
-import 'swiper/css';
+import '/swiper/css';
 
 const recipeSwiper = new Swiper('.recipe-swiper',{})
+console.log('testing new recipe UI')
