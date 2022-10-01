2cc150476e047a4bdbbf19bf926a8f2a99616848
Kristoff Lalicki <kristofflalicki@gmail.com>
2022-09-30 22:21:02 -0400

    adds basic CSS needed for Swiper to function

--- public/app/scripts/recipe-ui-new.js
+++ public/app/scripts/recipe-ui-new.js
@@ -1,6 +1,2 @@
-import Swiper from 'swiper';
-// import Swiper styles
-import '/swiper/css';
 
-const recipeSwiper = new Swiper('.recipe-swiper',{})
-console.log('testing new recipe UI')
+let recipeSwiper = new Swiper('.recipe-swiper')
